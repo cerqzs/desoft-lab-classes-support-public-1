2fa60c4576e31ed7554a07f76a66661a67477605
cerqzs <1222642@isep.ipp.pt>
2023-04-12 00:33:06 +0100

    ex9F

--- src/__tests__/_tests_03/ex9F.test.ts
+++ src/__tests__/_tests_03/ex9F.test.ts
@@ -1,13 +1,16 @@
 import { retornarArmstrong } from "../../lab03/functionslab3";
 
 describe(' retornar armstrong', () => {
-    test('verifica se existem numeros armstrong no intervalo 100 e 200  deve devolver 153', () => {
+    test('verifica se existem numeros armstrong no intervalo 100 e 200  deve devolver 153 e msg de erro qd nao existem numeros armstrong no intervalo', () => {
 
         //arrange
 
         const start:number=100;
         const end:number= 200;
+        const start1:number= 100;
+        const start2:number=150;
         const resultadoEsperado=153;
+    
         
 
 
@@ -19,6 +22,7 @@ describe(' retornar armstrong', () => {
         //assert
 
         expect(result).toBe(resultadoEsperado);
+        expect (()=> {retornarArmstrong(start1,start2)}).toThrow(new Error("Não existem numeros armstrong no intervalo"))
        
     })    
 })
